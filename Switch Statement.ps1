244eac20cce01c841c0b7ecb1156edf29db8225c
Justin Stevens <37387224+JustinSobo@users.noreply.github.com>
2021-03-13 14:11:24 -0500

    Create Switch Statement.ps1

---
+++ Switch Statement.ps1
@@ -0,0 +1,18 @@
+# Import CSV file and assign a varible to the returned data.
+$var_Name = Import-Csv .\file_Name.csv
+
+# Returns specific data within the CSV.
+$var_Name = $var_Name.Department[0]
+
+# Switch statement runs through each option below until an expression is true.
+# Executes only the actions contained within that true expression.
+# No actions performed if no statement is true.
+ 
+if ($var_Name -eq 'blank') {}
+elseif ($var_Name -eq 'blank') {}
+elseif ($var_Name -eq 'blank') {}
+elseif ($var_Name -eq 'blank') {}
+elseif ($var_Name -eq 'blank') {}
+elseif ($var_Name -eq 'blank') {}
+elseif ($var_Name -eq 'blank') {}
+elseif ($var_Name -eq 'blank') {}
