429828ce443df1092980c0a5be3fbe7d0592bca5
Justin Stevens <37387224+JustinSobo@users.noreply.github.com>
2021-10-12 02:52:25 -0400

    Update Get-ADUsers_LastLogonDate_GE_90_Days.ps1

--- Get-ADUsers_LastLogonDate_GE_90_Days.ps1
+++ Get-ADUsers_LastLogonDate_GE_90_Days.ps1
@@ -5,7 +5,7 @@ $pastDate = $currentDate.AddDays(-90)
 $attributes = @('Name', 'DisplayName', 'UserPrincipalName', 'LastLogonDate')
 $exportPath = '~\desktop\ADUsers_LastLogonDate_GE_90_Days.csv'
 
-# Retrieving ADUser for which have not been signed into for 90 or more days, and exporting the list to a CSV document.
+# Retrieving ADUser for which have not been signed in to for 90 or more days, and exporting the list to a CSV document.
 # EXAMPLE SEARCHBASE: <OU=NULL,DC=NULL,DC=NULL>
 Get-ADUser -Filter * -Properties * -SearchBase "<...>" `
 | Where-Object {$_.LastLogonDate -le $pastDate} `
